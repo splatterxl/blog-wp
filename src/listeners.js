1e390cc22e8913907ae3584202b2722efc811763
Splatterxl <splatterxl@outlook.ie>
2022-05-16 22:34:30 +0100

    try to make mobile navbar

--- src/listeners.js
+++ src/listeners.js
@@ -23,14 +23,19 @@ document.getElementById('hamburger').onclick = (event) => {
 
   event.preventDefault();
 
+  const navbar = document.getElementById('navbar');
   const inner = document.getElementById('nav-inner');
   const menu = document.getElementById('hamburger-icon');
 
   if (inner.style.display === 'block') {
     inner.style.display = 'none';
+    inner.hidden = true;
+    navbar.classList.remove('open');
     menu.textContent = 'menu';
   } else {
     inner.style.display = 'block';
+    inner.hidden = false;
+    navbar.classList.add('open');
     menu.textContent = 'menu_open';
   }
 };
