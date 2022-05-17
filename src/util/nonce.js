e5bad78f44b6b4a878b2b8f973fa08772af4aadf
Splatterxl <splatterxl@outlook.ie>
2022-05-17 17:58:40 +0100

    navbar updates again
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/util/nonce.js
+++ src/util/nonce.js
@@ -1,5 +1,7 @@
 import { debug } from "./logging.js";
 
-export const sessionNonce =
+export const sessionNonce = localStorage.getItem('session_nonce') ??
   crypto.randomUUID?.() ?? `${Date.now()}-${Math.random() * 30}`;
 debug("nonce", "sessionNonce:", sessionNonce);
+
+localStorage.setItem('session_nonce', sessionNonce);
